fb8e4ed1f4140afe5f83f43fe0339b093a68bd42
Riccardo Carbone <rcar32@student.monash.edu>
2017-08-04 18:16:47 +0200

    Added MS sendGrid example code

--- EmailSubsystem/Program.cs
+++ EmailSubsystem/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
 
 namespace EmailSubsystem
 {
@@ -6,7 +9,35 @@ namespace EmailSubsystem
     {
         static void Main(string[] args)
         {
+            Execute().Wait();
             Console.WriteLine("Hello World!");
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = "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";
+                //System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY"); Better code.
+            var client = new SendGridClient(apiKey);
+
+            
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("emailsubsystemtest@gmail.com", "Console App"),
+                Subject = "Hello World from the SendGrid CSharp SDK!",
+                //PlainTextContent = "Hello, Email!",
+                HtmlContent = 
+                "<html>" +
+                    "<body>" +
+                    "<a href='http://redsurveillance.azurewebsites.net/' title='RS Portal'><img src='cid:my_image' alt='My Image' border='0' /></a>" +
+                    "<br />" +
+                    "<h1>My E-mail Title</h1>" +
+                    "E-mail content." +
+                    "</body>" +
+                "</html>"
+        };
+            
+            msg.AddTo(new EmailAddress("rcar32@student.monash.edu", "Test User"));
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
